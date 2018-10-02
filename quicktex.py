292ac95f6bc2003cf5ecb6b55af434bee6bf1983
Cyrille Rossant <rossant@users.noreply.github.com>
2018-10-02 20:48:47 +0200

    Py2

--- quicktex.py
+++ quicktex.py
@@ -4,14 +4,12 @@ import os.path as op
 from pathlib import Path
 import subprocess
 import tempfile
-import sys
 
 from IPython.lib.latextools import genelatex, LaTeXTool
-#from PyQt5 import QtCore, QtGui, QtWidgets
-from flask import Flask, Response, request, render_template
+from flask import Flask, Response, render_template
 
 
-def cmd(c, *args, tmpdir=None):
+def cmd(c, args, tmpdir=None):
     with open(os.devnull, 'w') as devnull:
         try:
             subprocess.check_call(
@@ -28,32 +26,19 @@ def latex(content, tmpdir=None):
     with open(tmpfile, 'w') as f:
         f.writelines(lines)
     cmd('latex -halt-on-error %s',
-        tmpfile, tmpdir=tmpdir)
+        (tmpfile,), tmpdir=tmpdir)
     return outfile
 
 
 def dvi_ps(path):
     tmpdir = op.dirname(path)
-    cmd('dvips %s -o %s', path, Path(path).with_suffix('.ps'), tmpdir=tmpdir)
+    cmd('dvips %s -o %s', (path, Path(path).with_suffix('.ps')), tmpdir=tmpdir)
 
 
 def ps_pdf(path, output):
     tmpdir = op.dirname(path)
     cmd('gs -o %s -dNoOutputFonts -sDEVICE=pdfwrite -dEPSCrop %s',
-        output, path, tmpdir=tmpdir)
-
-
-def pdf_ps(path):
-    tmpdir = op.dirname(path)
-    cmd('pdf2ps %s', Path(path).with_suffix('.pdf'), path, tmpdir=tmpdir)
-
-
-def dvi_png(path):
-    tmpdir = op.dirname(path)
-    cmd('dvipng -T tight -x 6000 -z 9 -bg transparent -o %s %s',
-        Path(path).with_suffix('.png'),
-        Path(path).with_suffix('.dvi'),
-        tmpdir=tmpdir)
+        (output, path), tmpdir=tmpdir)
 
 
 def make_svg(content, preamble='', outfile=None):
@@ -61,30 +46,19 @@ def make_svg(content, preamble='', outfile=None):
     with tempfile.TemporaryDirectory() as tmpdir:
         tmpfile = latex(content, tmpdir=tmpdir)
         assert op.exists(tmpfile)
-        cmd('dvisvgm %s -o %s', tmpfile, outfile)
+        cmd('dvisvgm %s -o %s', (tmpfile, outfile))
         dvi_ps(tmpfile)
         psfile = Path(tmpfile).with_suffix('.ps')
         epsfile1 = Path(tmpfile).with_suffix('.eps')
         epsfile2 = Path(tmpfile).with_suffix('.tight.eps')
         assert op.exists(psfile)
-        cmd('ps2eps %s', psfile)
+        cmd('ps2eps %s', (psfile,))
         assert op.exists(epsfile1)
-        cmd('epstool --bbox --copy %s %s', epsfile1, epsfile2)
+        cmd('epstool --bbox --copy %s %s', (epsfile1, epsfile2))
         assert op.exists(epsfile2)
         pdffile = Path(tmpfile).with_suffix('.pdf')
         ps_pdf(epsfile2, pdffile)
-        cmd('pdf2svg %s %s', pdffile, outfile)
-
-
-"""
-def clipboard(path):
-    app = QtWidgets.QApplication([])
-    data = QtCore.QMimeData()
-    url = 'file://' + op.realpath(path)
-    data.setUrls([QtCore.QUrl(url)])
-    cb = app.clipboard()
-    cb.setMimeData(data)
-"""
+        cmd('pdf2svg %s %s', (pdffile, outfile))
 
 
 app = Flask(__name__)
